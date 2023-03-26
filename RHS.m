9b29d6f828b6fc28dc84e0d65d306f581d39c2f2
Reficul1234 <128955063+Reficul1234@users.noreply.github.com>
2023-03-25 22:40:16 -0700

    Add files via upload

---
+++ RHS.m
@@ -0,0 +1,2 @@
+function R=RHS(x,u,s);  E=Compute_E(x,s); N=Compute_N(x,u,s); R=E\N;
+end % function RHS
---
+++ Swap.m
@@ -0,0 +1,5 @@
+function [b,a]=Swap(a,b)
+% function [a,b]=Swap(a,b)
+% A curiously simple (empty!) function that swaps the contents of a and b.
+
+end % function Swap
